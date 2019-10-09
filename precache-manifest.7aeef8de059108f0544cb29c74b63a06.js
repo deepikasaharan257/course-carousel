666d7f21ed4e55ae4e7ed691c953431d6ec1f265
deepikasaharan257 <deepikasaharan257@gmail.com>
2019-10-08 21:26:00 -0600

    Updates

---
+++ precache-manifest.7aeef8de059108f0544cb29c74b63a06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2b4c204f4b94269d9f5b98705a4a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68da32ca5636e6709a1e",
+    "url": "/static/css/2.8622b7a2.chunk.css"
+  },
+  {
+    "revision": "a9c24975f71debc6d3d7",
+    "url": "/static/css/main.024a2d7c.chunk.css"
+  },
+  {
+    "revision": "68da32ca5636e6709a1e",
+    "url": "/static/js/2.8b6eac07.chunk.js"
+  },
+  {
+    "revision": "a9c24975f71debc6d3d7",
+    "url": "/static/js/main.ae22ccff.chunk.js"
+  },
+  {
+    "revision": "6a8d65e54010bb4cbb9a",
+    "url": "/static/js/runtime-main.64883d57.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
